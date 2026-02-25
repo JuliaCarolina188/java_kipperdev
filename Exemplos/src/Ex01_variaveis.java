aff214ab616f9ab9aab78203962f7afaa13013f4
Julia Carolina <julicarolyna@gmail.com>
2026-02-25 11:07:11 -0300

    Primeiros estudos

---
+++ Exemplos/src/Ex01_variaveis.java
@@ -0,0 +1,11 @@
+public class Ex01_variaveis {
+    public static void main() {
+        var nome = "Julia";
+
+        byte idade = 17;
+        float altura = 1.67f;
+        char sexo = 'f';
+        String sobreNome = "Ebert";
+        boolean matriculado = true;
+    }
+}
---
+++ Exemplos/src/Ex02_condicionais.java
@@ -0,0 +1,33 @@
+public class Ex02_condicionais {
+    public static void main() {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 10000000;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'a';
+        String str = "julia";
+        boolean bool = false;
+
+        if (bool) {
+            System.out.print("bool = verdadeiro");
+        } else {
+            System.out.println("bool = falso");
+        }
+
+        if (i < 0) {
+            System.out.println("int negativo");
+        } else if (i > 0 && i < 100) {
+            System.out.println("int positivo e menor que 100");
+        } else {
+            System.out.println("ins positivo maior que 100");
+        }
+
+        if (str == "aaaaa" || str.equals("julia")) {
+            System.out.println("AAAAAA");
+        } else {
+            System.out.println("a.");
+        }
+    }
+}
